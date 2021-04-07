9e47ba8ac6785b902a496a31fb0692cb5b1a7ad3
EkaterinaMolchanova <ekaterina.a.molchanova@gmail.com>
2021-04-07 03:05:52 +0300

    deploying zipfoods with homework

---
+++ zipfoods/src/components/pages/ProductPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="product-page">
+    <div class="show-product" v-for="product in products" v-bind:key="product.id">
+      <div v-if="product.id == id">
+        <h1>{{ product.name }}</h1>
+        <!-- <div class="name">{{ product.name }}</div> -->
+
+        <img class="thumb" v-bind:src="require('@/assets/images/products/' + product.id + '.jpg')" />
+
+        <div class="price">${{ product.price }}</div>
+        <p class="description">{{ product.description }}</p>
+      </div>
+    </div>
+    <div v-if="id > products.length">
+      <h1>Product {{ id }} not found</h1>
+      <p>
+        Return to the
+        <a href="/products">Products page</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { products } from "@/common/products.js";
+
+export default {
+  props: {
+    id: {
+      type: String
+    },
+    name: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      products: products
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ zipfoods/src/components/pages/ProductsPage.vue
@@ -0,0 +1,34 @@
+<!-- src/components/ShowProducts.vue -->
+<template>
+  <div id="products-page">
+    <h2>Products</h2>
+    <div id="products">
+      <show-product v-for="product in products" v-bind:key="product.id" v-bind:product="product"></show-product>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowProduct from "@/components/ShowProduct.vue";
+import { products } from "@/common/products.js";
+
+export default {
+  data() {
+    return {
+      products: products
+    };
+  },
+  components: {
+    "show-product": ShowProduct
+  }
+};
+</script>
+
+<style scoped>
+#products {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+}
+</style>
